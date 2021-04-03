f321f0c679d978f9a21ca656bd6f23bef37c9a5c
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2021-04-04 00:57:29 +0530

    create user model

---
+++ src/models/user.js
@@ -0,0 +1,46 @@
+'use strict';
+const mongoose = require('mongoose');
+const { Password } = require('../services/password');
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashedPassword = await Password.toHash(this.get('password'));
+    this.set('password', hashedPassword);
+  }
+  done();
+});
+
+userSchema.statics.build = (attrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User };
