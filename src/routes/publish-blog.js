e16962d5ad861174c3368089fd5845943c6eb4b9
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2021-04-04 01:00:01 +0530

    implement publish blog route

---
+++ src/routes/publish-blog.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const express = require('express');
+const { body } = require('express-validator');
+const { CustomError } = require('../errors/custom-error');
+const { Blog } = require('../models/blog');
+const router = express.Router();
+const { validateRequest } = require('../middleware/validate-request');
+const { requireAuth } = require('../middleware/require-auth');
+
+router.post(
+  '/',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('title must be valid'),
+    body('status').not().isEmpty().withMessage('status must be present'),
+    body('description')
+      .not()
+      .isEmpty()
+      .withMessage('description must be present'),
+  ],
+  validateRequest,
+  async (req, res) => {
+    const { title, status, body } = req.body;
+
+    const blog = Blog.build({
+      title,
+      status,
+      body,
+      author: req.currentUser.id,
+    });
+
+    await blog.save();
+
+    res.status(201).send(blog);
+  }
+);
