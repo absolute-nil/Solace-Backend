018497e42134ebaa9bb325630bb9a748e12117cc
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2021-04-03 23:42:56 +0530

    create error handler middleware

---
+++ src/middleware/error-handler.js
@@ -0,0 +1,12 @@
+const { Request, Response, NextFunction } = require('express');
+const { CustomError } = require('../errors/custom-error');
+
+const errorHandler = (err, req, res, next) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+  console.error(err);
+  res.status(400).send({ errors: [{ message: 'something went wrong' }] });
+};
+
+module.exports = { errorHandler };
