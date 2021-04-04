81861f3a93fa2922f495b128c2fceb7f5ffc2933
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2021-04-04 05:33:41 +0530

    add sockets

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 const { app } = require('./app.js');
+
+const server = require('http').Server(app);
+
+let io = require('socket.io')(server);
+
 const start = async () => {
   console.log('starting up ......');
   if (!process.env.JWT_KEY) {
@@ -15,6 +20,32 @@ const start = async () => {
       useCreateIndex: true,
     });
     console.log('connected to mongodb');
+
+    io.on('connection', (socket) => {
+      console.log('socket connected');
+
+      // for playing music .. pass the path and name of audio file
+      socket.on('play', (playMsg) => {
+        console.log('emitted play event');
+        io.emit('play', playMsg);
+      });
+
+      // for stopping the audio file
+      socket.on('stop', (stopMsg) => {
+        console.log('emitted stop event');
+        io.emit('stop');
+      });
+
+      // messages on the music channel (pass the message and the date)
+      socket.on('music_message', (message) => {
+        io.emit('music_message', message);
+      });
+
+      // messages on the channel message
+      socket.on('channel_message', (message) => {
+        io.emit('channel_message', message);
+      });
+    });
   } catch (e) {
     console.error(e);
   }
